283af9760824f01797fe26f80cca69e8791cbe11
chechooxd <sergiogalaz60@gmail.com>
2023-05-11 09:55:10 -0400

    refactorizacion de codigo

--- src/index.ts
+++ src/index.ts
@@ -1,85 +1,262 @@
 import * as Models from "./models";
 
-export const models = Models
+export const models = Models;
 
 export function createSelect(options: Models.ISelectOptions): Models.IResult {
+  const columns = getColumns(options.columns, Models.TypeQuery.Select);
+  const first = options.limit ? ` top ${options.limit} ` : " ";
 
-    const columns = getColumns(options.columns)
-    const first = options.limit ? ` top ${options.limit} ` : ' '
+  let result: Models.IResult = {
+    query: `select ${first}${columns} from ${options.table}`,
+  };
 
-    let result: Models.IResult = {
-        query: `select ${first}${columns} from ${options.table}`
-    }
+  if (options.join) {
+    result.query += getJoins(options.join);
+  }
 
-    if(options.join) {
-        result.query += getJoins(options.join)
-    }
+  if (options.condition) {
+    result.query += ` where ${getConditions(options.condition)}`;
+  }
 
-    if(options.condition){
-        result.query += ` where ${getConditions(options.condition)}` 
-    }
+  if (options.groupBy) {
+    result.query += ` group by ${options.groupBy.join(",")}`;
+  }
 
-    if(options.groupBy){
-        result.query += ` group by ${options.groupBy.join(',')}` 
-    }
+  if (options.orderBy) {
+    result.query += ` order by ${options.orderBy.join(",")} ${
+      options.orderByDirection ? options.orderByDirection : ""
+    }`;
+  }
 
-    if(options.orderBy){
-        result.query += ` order by ${options.orderBy.join(',')} ${options.orderByDirection ? options.orderByDirection : ''}` 
-    }
-
-    return result
+  return result;
 }
 
-function getJoins (joins:Models.IJoin | Models.IJoin[]) {
-    let result = []
-    
-    if (Array.isArray(joins)){
-        for (const join of joins){
-            result.push(`${join.type} join ${join.table} on ${getConditions(join.condition)}`)
-        }
-    }else{
-        result.push(`${joins.type} join ${joins.table} on ${getConditions(joins.condition)}`)
-    }
+function getJoins(joins: Models.IJoin | Models.IJoin[]) {
+  let result = [];
 
-    return result.length > 0 ? result.join(' ') : result[0]
+  if (Array.isArray(joins)) {
+    for (const join of joins) {
+      result.push(
+        `${join.type} join ${join.table} on ${getConditions(join.condition)}`
+      );
+    }
+  } else {
+    result.push(
+      `${joins.type} join ${joins.table} on ${getConditions(joins.condition)}`
+    );
+  }
+
+  return result.length > 0 ? result.join(" ") : result[0];
 }
 
-function getColumns(data?: Models.IColumn | Models.IColumn[]): string | string[] {
-    let dataColums: string[] | string = []
-    if (data)
+function getColumns(
+  data?: Models.IColumn | Models.IColumn[],
+  type?: Models.TypeQuery
+): string | string[] {
+  let dataColums: string[] | string = [];
+  if (data) {
+    switch (type) {
+      case Models.TypeQuery.Insert:
         if (Array.isArray(data)) {
-            for (const column of data) {
-                dataColums.push(`${column.name} as ${column.asigName ? column.asigName : column.name}`)
-            }
+          dataColums = data.map((column) => column.name).join(",");
         } else {
-            dataColums = `${data?.name} as ${data?.asigName ? data?.asigName : data?.name}`
+          dataColums = data.name;
         }
+        break;
+      case Models.TypeQuery.Select:
+        if (Array.isArray(data)) {
+          for (const column of data) {
+            dataColums.push(
+              `${column.name} ${column.asigName ? "as " + column.asigName : ""}`
+            );
+          }
+        } else {
+          dataColums = `${data?.name} ${
+            data?.asigName ? "as " + data?.asigName : ""
+          }`;
+        }
+        break;
+    }
+  }
 
-    return dataColums.length > 0 ? dataColums : '*'
+  return dataColums.length > 0 ? dataColums : "*";
 }
 
-function getConditions(conditionsGroup: Models.IGroupConditions | Models.IGroupConditions[]): string {
-    let result: string[] = []
+function getConditions(
+  conditionsGroup: Models.IGroupConditions | Models.IGroupConditions[]
+): string {
+  let result: string[] = [];
 
-    if (Array.isArray(conditionsGroup)) {
-        for (const conditions of conditionsGroup) {
-            let group: string[] = []
+  if (Array.isArray(conditionsGroup)) {
+    for (const conditions of conditionsGroup) {
+      let group: string[] = [];
 
-            for (const condition of conditions.groupConditions) {
-                group.push(`${condition.columnName} ${condition.operator} ${condition.value}`)
-            }
-
-            result.push(`( ${group.join(' and ')} )`)
+      for (const condition of conditions.groupConditions) {
+        const { columnName, operator, value, values } = condition;
+        switch (operator) {
+          case Models.Operator.In:
+            const valuesReturn = values.map((value) =>
+              validateValue({ value })
+            );
+            group.push(`${columnName} ${operator} (${valuesReturn.join(",")})`);
+            break;
+          case Models.Operator.Like:
+            group.push(`${columnName} ${operator} '%${value}%'`);
+            break;
+          case Models.Operator.between:
+            group.push(
+              `${columnName} ${operator} ${validateValue({
+                value: values[0],
+              })} and ${validateValue({ value: values[1] })}`
+            );
+            break;
+          default:
+            group.push(`${columnName} ${operator} ${validateValue({ value })}`);
+            break;
         }
+        group.push(`${columnName} ${operator} ${validateValue({ value })}`);
+      }
 
-    } else {
-        let group: string[] = []
+      result.push(`( ${group.join(" and ")} )`);
+    }
+  } else {
+    let group: string[] = [];
 
-        for (const condition of conditionsGroup.groupConditions) {
-            group.push(`${condition.columnName} ${condition.operator} ${condition.value}`)
-        }
-        result.push(`( ${group.join(' and ')} )`)
+    for (const condition of conditionsGroup.groupConditions) {
+      group.push(
+        `${condition.columnName} ${condition.operator} ${condition.value}`
+      );
+    }
+    result.push(`( ${group.join(" and ")} )`);
+  }
+
+  return result.length > 0 ? result.join(" or ") : result[0];
+}
+
+export function createInsert(options: Models.IInsertOptions): Models.IResult {
+  const table = options.table;
+  const columns = getColumns(options.columns, Models.TypeQuery.Insert);
+  const values: string = getValidateValues(
+    Array.isArray(options.values) ? options.values : [options.values]
+  );
+
+  let result: Models.IResult = {
+    query: `insert into ${table} (${columns}) values (${values})`,
+  };
+
+  return result;
+}
+
+export function createUpdate(options: Models.IUpdateOptions): Models.IResult {
+  const { table, columns, values, condition } = options;
+
+  let result: Models.IResult = {
+    query: `update ${table}`,
+  };
+
+  if (Array.isArray(columns) && Array.isArray(values)) {
+    let set: string[] = [];
+
+    for (let i = 0; i < columns.length; i++) {
+      set.push(`${columns[i]} = ${validateValue(values[i])}`);
     }
 
-    return result.length > 0 ? result.join(' or ') : result[0]
+    result.query += ` set ${set.join(", ")}`;
+  } else {
+    const value = Array.isArray(values)
+      ? validateValue(values[0])
+      : validateValue(values);
+    result.query += ` set ${columns} = ${value}`;
+  }
+
+  if (condition) {
+    result.query += ` where ${getConditions(condition)}`;
+  }
+
+  return result;
+}
+
+export function createDelete(options: Models.IDeleteOptions): Models.IResult {
+  const table = options.table;
+  const condition = options.condition ? getConditions(options.condition) : "";
+
+  let result: Models.IResult = {
+    query: `delete from ${table}`,
+  };
+
+  if (options.condition) {
+    result.query += ` where ${condition}`;
+  }
+
+  return result;
+}
+
+export function CreateTable(
+  options: Models.ICreateTableOptions
+): Models.IResult {
+  const { table, columns } = options;
+
+  let result: Models.IResult = {
+    query: `create table ${table} (`,
+  };
+
+  let columnsQuery: string[] = [];
+
+  for (const column of columns) {
+    let columnQuery = `${column.name} ${column.type}`;
+
+    if (column.primaryKey) {
+      columnQuery += " primary key";
+    }
+
+    if (column.autoIncrement) {
+      columnQuery += " identity(1,1)";
+    }
+
+    if (column.notNull) {
+      columnQuery += " not null";
+    }
+
+    if (column.unique) {
+      columnQuery += " unique";
+    }
+
+    if (column.defaultValue) {
+      columnQuery += ` default ${validateValue({
+        value: column.defaultValue,
+      })}`;
+    }
+
+    if (column.foreignKey) {
+      columnQuery += ` foreign key references ${column.foreignKey.table} (${column.foreignKey.column})`;
+    }
+
+    columnsQuery.push(columnQuery);
+  }
+
+  result.query += columnsQuery.join(", ") + ")";
+
+  return result;
+}
+
+function validateValue(value: Models.ITypeValue): string {
+  if (typeof value === "string") {
+    return `'${value}'`;
+  } else if (typeof value === "number") {
+    return `${value}`;
+  } else if (typeof value === "boolean") {
+    return `${value ? 1 : 0}`;
+  } else {
+    return `${value}`;
+  }
+}
+
+function getValidateValues(values: Models.ITypeValue[]): string {
+  let result: string[] = [];
+
+  for (const value of values) {
+    result.push(validateValue(value));
+  }
+
+  return result.join(",");
 }
--- src/models.ts
+++ src/models.ts
@@ -1,62 +1,159 @@
 export interface ISelectOptions {
-    table: string,
-    columns?: IColumn[] | IColumn,
-    condition?: IGroupConditions | IGroupConditions[],
-    limit?: number,
-    groupBy?: string[],
-    orderBy?: string[],
-    join?: IJoin | IJoin[],
-    orderByDirection?: OrderByDirection
+  table: string;
+  columns?: IColumn[] | IColumn;
+  condition?: IGroupConditions | IGroupConditions[];
+  limit?: number;
+  groupBy?: string[];
+  orderBy?: string[];
+  join?: IJoin | IJoin[];
+  orderByDirection?: OrderByDirection;
 }
 
 export interface IColumn {
-    name: string,
-    asigName?: string
+  name: string;
+  asigName?: string;
+}
+
+export interface IColumnCreate {
+  name: string;
+  type: TypeColumn;
+  length?: number;
+  notNull?: boolean;
+  primaryKey?: boolean;
+  autoIncrement?: boolean;
+  unique?: boolean;
+  defaultValue?: string;
+  check?: string;
+  foreignKey?: IForeignKey;
+}
+
+export interface IForeignKey {
+  table: string;
+  column: string;
 }
 
 export interface IJoin {
-    table:string,
-    condition: IGroupConditions | IGroupConditions[],
-    type: TypeJoin
+  table: string;
+  condition: IGroupConditions | IGroupConditions[];
+  type: TypeJoin;
 }
 
 export interface ICondition {
-    columnName: string,
-    operator: Operator
-    value: string | number,
+  columnName: string;
+  operator: Operator;
+  value: string | number | boolean;
+  values: string[] | number[] | boolean[];
 }
 
 export interface IGroupConditions {
-    groupConditions: ICondition[]
+  groupConditions: ICondition[];
 }
 
 export interface IParams {
-    name: string,
-    value: string | number
+  name: string;
+  value: string | number;
 }
 
 export interface IResult {
-    query: string,
-    params?: IParams[]
+  query: string;
+  params?: IParams[];
 }
 
 export enum Operator {
-    Equals = "=",
-    Distinct = "<>",
-    In = "in",
-    Like = "like",
-    greaterThanOrEqual = ">=",
-    lessThanOrEqual = "<="
+  Equals = "=",
+  Distinct = "<>",
+  In = "in",
+  Like = "like",
+  greaterThanOrEqual = ">=",
+  lessThanOrEqual = "<=",
+  between = "between",
 }
 
 export enum OrderByDirection {
-    ASC = "asc",
-    DESC = "desc"
+  ASC = "asc",
+  DESC = "desc",
 }
 
 export enum TypeJoin {
-    Inner = 'inner',
-    Right = 'right',
-    Left = 'left',
-    Full = 'full outer'
+  Inner = "inner",
+  Right = "right",
+  Left = "left",
+  Full = "full outer",
+}
+
+export enum TypeColumn {
+  String = "varchar",
+  Number = "int",
+  Date = "date",
+  DateTime = "datetime",
+  Boolean = "bit",
+  Decimal = "decimal",
+  Float = "float",
+  Double = "double",
+  Text = "text",
+  LongText = "longtext",
+  MediumText = "mediumtext",
+  TinyText = "tinytext",
+  Blob = "blob",
+  LongBlob = "longblob",
+  MediumBlob = "mediumblob",
+  TinyBlob = "tinyblob",
+  Binary = "binary",
+  VarBinary = "varbinary",
+  Char = "char",
+  VarChar = "varchar",
+  Time = "time",
+  Year = "year",
+  Timestamp = "timestamp",
+  Enum = "enum",
+  Set = "set",
+  Geometry = "geometry",
+  Point = "point",
+  LineString = "linestring",
+  Polygon = "polygon",
+  MultiPoint = "multipoint",
+  MultiLineString = "multilinestring",
+  MultiPolygon = "multipolygon",
+  GeometryCollection = "geometrycollection",
+  Json = "json",
+  TinyInt = "tinyint",
+  SmallInt = "smallint",
+  MediumInt = "mediumint",
+  Int = "int",
+  BigInt = "bigint",
+}
+
+export interface ITypeValue {
+  value: string | number | boolean;
+}
+
+export interface IInsertOptions {
+  table: string;
+  columns: IColumn[] | IColumn;
+  values: ITypeValue[] | ITypeValue;
+}
+
+export interface IUpdateOptions {
+  table: string;
+  columns: IColumn[] | IColumn;
+  values: ITypeValue[] | ITypeValue;
+  condition?: IGroupConditions | IGroupConditions[];
+}
+
+export interface IDeleteOptions {
+  table: string;
+  condition?: IGroupConditions | IGroupConditions[];
+}
+
+export interface ICreateTableOptions {
+  table: string;
+  columns: IColumnCreate[];
+}
+
+export enum TypeQuery {
+  Select = "select",
+  Insert = "insert",
+  Update = "update",
+  Delete = "delete",
+  CreateTable = "create table",
 }
